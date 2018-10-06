8899e77f9245484cb6b8b3cc3c58202efd0f92a9
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2018-10-06 08:28:15 -0300

    Update switch.asm

--- switch.asm
+++ switch.asm
@@ -111,3 +111,20 @@ _start:
 
   write_string msg3, len3
   exit
+
+; Older version -----
+
+;%macro switch 1
+;  %push switch
+;  %define arg %1
+;%endmacro
+
+;%macro case 1
+;  cmp %1, arg
+;  jne %$end_case
+;%endmacro
+
+;%macro end_case
+;  %$end_case:
+;    %pop
+;%endmacro
